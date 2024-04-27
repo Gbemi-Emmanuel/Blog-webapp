4a8dfea756cb2f10ee1beeadcc7c0107d0963b4c
unknown <gbemiemmanuel555@gmail.com>
2024-04-27 13:33:22 +0100

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import About from './pages/About'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './component/Header'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,20 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.{js,ts,jsx,tsx}'
+    flowbite.content(),
+    // 'node_modules/flowbite-react/lib/esm/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    // require('flowbite/plugin')
+    flowbite.plugin(),
 ]
 }
 
