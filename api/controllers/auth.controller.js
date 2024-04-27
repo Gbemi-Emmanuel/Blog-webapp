c96948c874b8e19b9dd1b7efb68a718d705d9d84
unknown <gbemiemmanuel555@gmail.com>
2024-04-27 17:25:24 +0100

    complete sign up page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async ( req, res ) => {
+export const signup = async ( req, res, next ) => {
     const { username, email, password } = req.body;
 
     if (!username || 
@@ -10,7 +11,7 @@ export const signup = async ( req, res ) => {
         username === '' || 
         email === '' || 
         password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -25,7 +26,7 @@ export const signup = async ( req, res ) => {
         await newUser.save();
         res.json('Signup successfull');
     } catch (error) {
-        res.status(500).json({message: error.message})
+        next(error);
     }
 
 
